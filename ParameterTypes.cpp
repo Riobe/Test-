90a7f3efe8b12d8238a1ba30e4ce671bc609de4f
Jeremy Pridemore <jeremy.pridemore@gmail.com>
2015-06-25 12:49:03 -0600

    Initial class for ParameterTypesConcept.

---
+++ ParameterTypes.cpp
@@ -0,0 +1,11 @@
+#include "ParameterTypes.hpp"
+
+#include <iostream>
+
+using namespace std;
+using namespace concept;
+
+void ParamaterTypesConcept::run()
+{
+
+}
---
+++ ParameterTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef PARAMETERTYPESCONCEPT_HPP
+#define PARAMETERTYPESCONCEPT_HPP
+
+#include "IConcept.hpp"
+
+namespace concept
+{
+    class ParamaterTypesConcept : public IConcept
+    {
+    public:
+        virtual void run();
+    };
+}
+
+#endif // !PARAMETERTYPESCONCEPT_HPP
