0170c78ebc7ea54baa88d6138822162c48d5c2dc
Jeremy Pridemore <jeremy.pridemore@gmail.com>
2015-06-25 12:42:27 -0600

    Adding empty concept files for desired concepts.

--- Program.cpp
+++ Program.cpp
@@ -1,15 +1,40 @@
 #include <iostream>
-#include "HelloWorldConcept.hpp"
+
+// TODO: Implement ArrayAndVectorConcept
+//#include "ArrayAndVectorConcept.hpp"
+
+// TODO: Implement BasicFileIO
+//#include "BasicFileIO.hpp"
+
 #include "ConsoleInputConcept.hpp"
+
+// TODO: Implement FunctionPointerConcept
+//#include "FunctionPointerConcept.hpp"
+
+// TODO: Implement HelloWorldConcept
+//#include "HelloWorldConcept.hpp"
+
+// TODO: Implement MapAndUnorderedMapConcept
+//#include "MapAndUnorderedMapConcept.hpp"
+
+// TODO: Implement MemoryManagementConcept
+//#include "MemoryManagementConcept.hpp"
+
+// TODO: Implement ParameterTypes
+//#include "ParameterTypes.hpp"
+
 
 using namespace std;
 using namespace concept;
 
+int x;
 int main()
 {
     IConcept *concept = new ConsoleInputConcept();
 
     concept->run();
+
+    cout << x;
 
     cout << endl << "Press enter to finish...";
     
